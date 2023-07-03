2777623a5afcedc183c86b06c2461234e424a3d0
christain chidi okeke <chidiokeken@gmail.com>
2023-07-03 03:33:04 +0100

    added flex box for for login and logut

--- website/__init__.py
+++ website/__init__.py
@@ -18,10 +18,10 @@ uri = os.environ.get('DATABASE_URL')
 if uri.startswith('postgres://'):
     uri = uri.replace('postgres://', 'postgresql://', 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(base_dir, 'scissor.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    # os.path.join(base_dir, 'scissor.db')
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
